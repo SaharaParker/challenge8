a3db07d36eb8b25b46abe69169a9c2ee7073eade
SaharaParker <93682682+SaharaParker@users.noreply.github.com>
2022-03-09 02:18:38 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Searchbar from "./Searchbar";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Journal App</h1>
+      <h2>Making something quick and interactive!</h2>
+
+      <Searchbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SearchForm.js
@@ -0,0 +1,9 @@
+function Searchbar() {
+  return (
+    <div>
+      <input type="text" placeholder="Search App Here!" />
+    </div>
+  );
+}
+
+export default Searchbar;
